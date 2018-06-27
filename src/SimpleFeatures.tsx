c6038b43d3c045f9c271ef557b234de28d94c003
Can Nahum <can_nahum@intuit.com>
2018-06-26 18:05:48 -0700

    First simple feature flags are working.

--- src/SimpleFeatures.tsx
+++ src/SimpleFeatures.tsx
@@ -1,18 +1,57 @@
 import React, {Component} from 'react';
 import JSXFeatures from './simple/JSXFeatures';
 import TSXFeatures from './simple/TSXFeatures';
+import {FeatureFlagHelper} from '../lib/feature-helper/';
 
 interface ISimpleFeaturesProps {
   lovesTypeScript: boolean;
 }
-export default class SimpleFeatures extends Component<ISimpleFeaturesProps> {
+
+interface ISimpleFeaturesState {
+  isFetched: boolean;
+}
+
+export default class SimpleFeatures extends Component<ISimpleFeaturesProps, ISimpleFeaturesState> {
+
+  constructor(props: ISimpleFeaturesProps) {
+    super(props);
+
+    this.state = {
+      isFetched: false,
+    };
+  }
+
+  componentWillMount() {
+    const source = {
+      sourceName: 'pureString',
+      fetcher: pureStringFetcher,
+    };
+    const ffh: any = FeatureFlagHelper.getInstance();
+    ffh.addFeatureSource(source);
+    ffh.fetchSources()
+      .then(() => {
+        this.setState({
+          isFetched: true
+        });
+      })
+  }
 
   render() {
     return (
       <div>
-        <TSXFeatures firstName={"Jon"} lovesTypeScript={this.props.lovesTypeScript}/>
+        <TSXFeatures
+          firstName={'Jon'}
+          lovesTypeScript={this.props.lovesTypeScript}
+          flagsAreFetched={this.state.isFetched}/>
         <JSXFeatures/>
       </div>
     );
   }
 }
+
+const pureStringFetcher = (): Promise<Array<string>> => {
+  return new Promise((resolve) => resolve([
+    'featureA',
+    'featureC',
+  ]));
+};
--- src/simple/TSXFeatures.tsx
+++ src/simple/TSXFeatures.tsx
@@ -1,17 +1,42 @@
 import React from 'React';
+import {FeatureFlagHelper} from '../../lib/feature-helper/';
+const ffh: any = FeatureFlagHelper.getInstance();
 
 interface IFeatureProps {
   firstName: string;
   lovesTypeScript: boolean;
+  flagsAreFetched: boolean;
 }
 
 export default class TSXFeatures extends React.Component<IFeatureProps> {
+
   render() {
-    const {firstName, lovesTypeScript} = this.props;
+    const {firstName, lovesTypeScript, flagsAreFetched} = this.props;
+    const getFeatureComponent = (name: string): JSX.Element => {
+      const isOpen: boolean = ffh.isFeaturePublished(name);
+      if (isOpen) {
+        return <p style={{backgroundColor: '#00800085'}}>{name}</p>
+      }
+      return <p style={{backgroundColor: '#ff000075'}}>{name}</p>
+    };
     return (
       <div>
         Welcome {firstName}!
         You currently {lovesTypeScript ? 'love' : 'does not love'} TS.
+        {
+          flagsAreFetched
+            ? (
+              <div>
+                {getFeatureComponent('featureA')}
+                {getFeatureComponent('featureB')}
+                {getFeatureComponent('featureC')}
+                {getFeatureComponent('featureD')}
+              </div>
+            )
+            : (
+              <p>Loading...</p>
+            )
+        }
       </div>
     );
   }
